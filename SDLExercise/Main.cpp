5fbeac2563503e201386fe9f952ea696790987de
AxelAlavedra <axel.alavedra@gmail.com>
2018-03-06 11:23:43 +0100

    Base 2

--- SDLExercise/Main.cpp
+++ SDLExercise/Main.cpp
@@ -1,12 +1,12 @@
 #include "../SDL/include/SDL.h"
-#pragma comment(lib,"../SDL/lib/x86/SDL2.lib")
-#pragma comment(lib,"../SDL/lib/x86/SDL2main.lib")
+#pragma comment(lib,"../SDL/libx86/SDL2.lib")
+#pragma comment(lib,"../SDL/libx86/SDL2main.lib")
 
 #include "../SDL_Image/include/SDL_image.h"
-#pragma comment(lib,"../SDL_Image/lib/x86/SDL2_image.lib")
+#pragma comment(lib,"../SDL_Image/libx86/SDL2_image.lib")
 
 #include "../SDL_Mixer/include/SDL_mixer.h"
-#pragma comment(lib,"../SDL_Mixer/lib/x86/SDL2_mixer.lib")
+#pragma comment(lib,"../SDL_Mixer/libx86/SDL2_mixer.lib")
 
 
 #define WIDTH 640
